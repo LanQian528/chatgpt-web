1b8d58f6baf7e2728b39f1960283726948412bd1
LanQian <5499636+LanQian528@users.noreply.github.com>
2024-05-21 01:52:20 +0800

    del github-site

--- src/components/common/GitHubSite/index.vue
+++ src/components/common/GitHubSite/index.vue
@@ -1,7 +1,7 @@
-<script setup lang="ts">
-const gitCommitSha = (import.meta.env.VITE_GIT_COMMIT_HASH || 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx').slice(0, 7)
-const releaseVersion = import.meta.env.VITE_RELEASE_VERSION || 'v0.0.0'
-</script>
+<!--<script setup lang="ts">-->
+<!--const gitCommitSha = (import.meta.env.VITE_GIT_COMMIT_HASH || 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx').slice(0, 7)-->
+<!--const releaseVersion = import.meta.env.VITE_RELEASE_VERSION || 'v0.0.0'-->
+<!--</script>-->
 
 <!--<template>-->
 <!--  <div class="text-center text-xs">-->
